f5b2cc2ab62c34d5684e6a1765009f28aa876ba7
Alexandre Gama <login+alexandregama@elo7.com>
2014-12-30 11:43:09 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalCar::Application.config.secret_token = 'ad873faedda5ab47cfc220567d409d60511ae9beb29d969d8c1c39c776a307c157084081ced41fc42adb67e39195a596a1c56e015594dff422038a4fa406ff5c'
